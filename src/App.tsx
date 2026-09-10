2c26776bdfc9d8e121c6eacaa6263ee04350875b
hakan-gleissman <hakan.gleissman@gmail.com>
2026-09-10 10:25:27 +0200

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,27 @@ import Section from "./components/Section";
 import Article from "./components/Article";
 import type { ArticleType, NewArticle } from "./types/Article";
 import ArticleForm from "./components/ArticleForm";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+// const articlesList: ArticleType[] = [
+//   {
+//     id: 1,
+//     title: "Artikel här",
+//     description: "Beskrivning",
+//     category: "news",
+//   },
+//   {
+//     id: 2,
+//     title: "Blogg här",
+//     description: "Beskrivning",
+//     category: "blog",
+//   },
+// ];
 
-const articlesList: ArticleType[] = [
-  {
-    id: 1,
-    title: "Första nyheten",
-    category: "news",
-    description: "Beskrivning..",
-  },
-  {
-    id: 2,
-    title: "Första bloggen",
-    category: "blog",
-    description: "Beskrivning..",
-  },
-];
+const apiUrl = "http://localhost:3005/api/articles";
 
 const App = () => {
   const [articleId, setArticleId] = useState(3);
-  const [articles, setArticles] = useState<ArticleType[]>(articlesList);
+  const [articles, setArticles] = useState<ArticleType[]>([]);
 
   const news: ArticleType[] = articles.filter(
     (article) => article.category === "news",
@@ -30,13 +31,41 @@ const App = () => {
     (article) => article.category === "blog",
   );
 
-  const addArticle = (newArticle: NewArticle) => {
-    const article: ArticleType = {
-      id: articleId,
-      ...newArticle,
+  const fetchArticles = async () => {
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error("Kunde inte hämta");
+      }
+      const result: ArticleType[] = await response.json();
+      setArticles(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    const executeFetch = async () => {
+      await fetchArticles();
     };
-    setArticleId(articleId + 1);
-    setArticles([...articles, article]);
+    executeFetch();
+  }, []);
+
+  const addArticle = async (newArticle: NewArticle) => {
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newArticle),
+      });
+      if (!response.ok) {
+        throw new Error("Kunde inte skapa artikel");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    await fetchArticles();
   };
   return (
     <main>
